40e5b27503d9cb0bd2de7f44294dacc8bb090da4
Maya Bowman <mayashady1@gmail.com>
2019-08-03 10:28:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const apiKey = 'Wuo64AKtlhqtUJFBpSwNrkFvZcXygXkf';
+
+const searchURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+
+// format query parameters
+function formatQueryParams(params) {
+  console.log(params);
+  const queryItems = Object.keys(params).map(key => `${key}=${params[key]}`)
+  console.log(queryItems);
+  return queryItems.join('&');
+}
+
+// retrieve data from nyt api
+function getNews(searchDate) {
+  const params = {
+    pub_date: searchDate
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString + '&' + 'api-key=' + apiKey;
+
+  console.log(url);
+
+  fetch(url)
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => displayResults(responseJson))
+  .catch(error => {
+    $('#error-message').text(`Something went wrong: ${error.message}`);
+  });
+}
+
+// display results
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#error-message').empty();
+  $('#results').empty();
+  
+}
+
+// event listener
+function watchForm() {
+  $('#search-form').submit(event => {
+    event.preventDefault();
+    const searchDate = $('#search-date').val();
+    getNews(searchDate);
+  });
+}
+
+$(watchForm);
