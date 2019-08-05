b589da38d9e4832888df7a0f193ac0e0285712fc
Maya Bowman <mayashady1@gmail.com>
2019-08-05 10:50:23 -0400

    added function for displaying articles from 50 years ago today

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
+// retrieve headlines from today's date -50 years from nyt api
+function getOldNews(autoDate) {
+  const params = {
+    pub_date: autoDate
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString + '&' + 'api-key=' + apiKey;
+
+  console.log(url);
+
+  fetch(url)
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => displayAutoResults(responseJson))
+  .catch(error => {
+    $('#error-message').text(`Something went wrong: ${error.message}`);
+  });
+}
+
 // retrieve data from nyt api
 function getNews(searchDate) {
   const params = {
@@ -33,6 +56,17 @@ function getNews(searchDate) {
   .catch(error => {
     $('#error-message').text(`Something went wrong: ${error.message}`);
   });
+}
+
+// display auto results
+function displayAutoResults(responseJson) {
+  console.log(responseJson);
+  $('#error-message').empty();
+  for (let i = 0; i < responseJson.response.docs.length; i++) {
+    $('#feature-article').append(`<li><a href='${responseJson.response.docs[i].web_url}'>${responseJson.response.docs[i].headline.main}</a></li>`);
+  };
+  $('#search-date').val('');
+  
 }
 
 // display results
@@ -58,4 +92,26 @@ function watchForm() {
   });
 }
 
+// event listener for feature article
+function watchPage() {
+  $(document).ready(event=> {
+    // event.preventDefault();
+    let today = new Date();
+    let date = today.getDate();
+    let month = today.getMonth() +1;
+
+    if (month < 10) {
+      month = '0' + month;
+    }
+
+    if (date < 10) {
+      date = '0' + date;
+    }
+
+    let autoDate = month + date + today.getFullYear() - 50;
+    getOldNews(autoDate);
+  });
+}
+
 $(watchForm);
+$(watchPage);
